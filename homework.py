a8be0600a9aaf1bb6b0832c69328679bd5b7e7e6
ViktoriaPashkovskaja <pashkovskajava@yandex.ru>
2021-07-30 12:30:43 +0300

    первая попытка сдать

--- homework.py
+++ homework.py
@@ -1,25 +1,27 @@
+import logging
 import os
 import time
 import requests
 import telegram
-from dotenv import load_dotenv
 
+from dotenv import load_dotenv
+from logging.handlers import RotatingFileHandler
+from telegram.ext import Updater
 
 load_dotenv()
 
 PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-
-# проинициализируйте бота здесь,
-# чтобы он был доступен в каждом нижеобъявленном методе,
-# и не нужно было прокидывать его в каждый вызов
-bot = ...
+bot = telegram.Bot(token=TELEGRAM_TOKEN)
+url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+updater = Updater(TELEGRAM_TOKEN, use_context=True)
 
 
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
+    homework_name = homework.get('homework_name')
+    status = homework.get('status')
+    if status == 'rejected':
         verdict = 'К сожалению, в работе нашлись ошибки.'
     else:
         verdict = 'Ревьюеру всё понравилось, работа зачтена!'
@@ -27,26 +29,52 @@ def parse_homework_status(homework):
 
 
 def get_homeworks(current_timestamp):
-    homework_statuses = ...
-    return homework_statuses.json()
+    if current_timestamp is None:
+        current_timestamp = int(time.time())
+    headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
+    params = {'from_date': current_timestamp}
+    try:
+        homework_statuses = requests.get(
+            url, headers=headers, params=params)
+        return homework_statuses.json()
+    except Exception as e:
+        print(f'Ошибка у бота {e}')
+        return dict()
 
 
 def send_message(message):
-    return bot.send_message(...)
+    return bot.send_message(CHAT_ID, message)
 
 
 def main():
     current_timestamp = int(time.time())  # Начальное значение timestamp
 
+    logging.basicConfig(
+        level=logging.DEBUG,
+        filename='program.log',
+        format='%(asctime)s, %(levelname)s, %(message)s, %(name)s'
+    )
+
     while True:
         try:
-            ...
+            new_homework = get_homeworks(current_timestamp)
+            if new_homework.get('homeworks'):
+                send_message(parse_homework_status(new_homework.get('homeworks')[0]))
+            current_timestamp = new_homework.get('current_date', current_timestamp)
             time.sleep(5 * 60)  # Опрашивать раз в пять минут
 
         except Exception as e:
             print(f'Бот упал с ошибкой: {e}')
             time.sleep(5)
+            site_error = f'Ошибка на API - json():{e}.'
+            logging.error(site_error)
+            return bot.send_message(site_error)
 
 
 if __name__ == '__main__':
     main()
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = RotatingFileHandler('my_logger.log', maxBytes=50000000, backupCount=5)
+logger.addHandler(handler)
